d0d21ee4f61119e058d88336abf3506518167f70
Brett Csotty <brett.csotty@gmail.com>
2023-11-06 11:52:38 -0500

    Extracted common CSS, finished login page, removed unused assets.

--- personalpalate/static/main.js
+++ personalpalate/static/main.js
@@ -1,6 +1,13 @@
-var returnToSignIn = document.getElementById("returnToSignIn");
+const returnToSignIn = document.getElementById("returnToSignIn");
 if (returnToSignIn) {
     returnToSignIn.addEventListener("click", function (e) {
         window.location.href = "/login";
+    });
+}
+
+const returnToSignUp = document.getElementById("returnToSignUp");
+if (returnToSignUp) {
+    returnToSignUp.addEventListener("click", function (e) {
+        window.location.href = "/sign-up";
     });
 }
