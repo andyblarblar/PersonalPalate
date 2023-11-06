24fbc58f394847acd05c1881b78f22ebab41d041
Joseph Smiley <smileyjo@umich.edu>
2023-11-06 15:27:04 -0500

    update model.py
    added database relations

--- personalpalate/orm/model.py
+++ personalpalate/orm/model.py
@@ -1,13 +1,46 @@
+from datetime import date
 from typing import Optional
 
 from sqlmodel import SQLModel, Field
 from sqlalchemy import PrimaryKeyConstraint
 
 
-# TODO add model
 class AccountDTO(SQLModel):
-    email: str = Field(primary_key=True)
-
+    password: str
 
 class Account(AccountDTO, table=True):  # Subclass to avoid sending password to client
-    password: str
+    email: str = Field(primary_key=True)
+    followable: bool
+
+class Follow(SQLModel, table=True):
+    email: str = Field(default=None, foreign_key="account.email")
+    followingEmail: str = Field(default=None, foreign_key="account.email")
+
+    __table_args__ = (
+        PrimaryKeyConstraint("email", "followingEmail"),
+    )
+
+class Meal(SQLModel, table=True):
+    mealID: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(default=None, foreign_key="account.email")
+    mealName: str
+    category: str
+    description: str
+    mealFrequency: int
+    dateMade: date
+
+class Ingredients(SQLModel, table=True):
+    ingredientID: Optional[int] = Field(default=None, primary_key=True)
+    mealID: int = Field(default=None, foreign_key="meal.mealID")
+    ingredient: str
+    quantity: str
+
+class MealPlan(SQLModel, table=True):
+    mealPlanID: int = Field(default=None, primary_key=True)
+    mealPlanDate: date
+    email: str = Field(default=None, foreign_key="account.email")
+
+class Day(SQLModel, table=True):
+    dayID: Optional[int] = Field(default=None, primary_key=True)
+    mealID: int = Field(default=None, foreign_key="meal.mealID")
+    mealPlanID: int = Field(default=None, foreign_key="mealplan.mealPlanID")
