9e733c056a9ff33c580d0b2fafdc992da58186ce
Brett Csotty <brett.csotty@gmail.com>
2023-11-14 13:11:57 -0500

    Fixed CSS so Login flows properly

--- personalpalate/static/js/user.js
+++ personalpalate/static/js/user.js
@@ -11,3 +11,26 @@ if (returnToSignUp) {
         window.location.href = "/sign-up";
     });
 }
+
+const form = document.getElementById("login");
+if (form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        fetch(form.action,
+            {
+                method: "post",
+                body: new FormData(form)
+            }).then(
+                r => {
+                    if (!r.ok) {
+                        //Invalid username/password
+                        const invalid = document.getElementById("invalid");
+                        invalid.toggleAttribute("hidden");
+                    } else {
+                        window.location = "/";
+                    }
+                }
+        );
+    });
+}
