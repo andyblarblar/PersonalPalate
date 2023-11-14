537c9fca04d51c4178009caaa57c4dcd37105425
Joseph Smiley <smileyjo@umich.edu>
2023-11-14 16:44:05 -0500

    more updated db and sample data

--- personalpalate/orm/connect.py
+++ personalpalate/orm/connect.py
@@ -5,7 +5,7 @@ from .model import *  # Required to create tables
 sqlite_file_name = "db.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url, echo=True, connect_args={"check_same_thread": False})
 
 
 def prepare_db():
@@ -14,39 +14,126 @@ def prepare_db():
 
     with Session(engine) as sess:
         # Only generate once
-        if not sess.exec(select(Account).where(Account.email == "avealov@umich.edu")).first():
-            # TODO add premade model
-            a1 = Account(email="avealov@umich.edu", password=password_context.hash("123"))
-            a2 = Account(email="smileyjo@umich.edu", password=password_context.hash("456"), followable=1)
-            a3 = Account(email="joeysmiley33@gmail.com", password=password_context.hash("789"), followable=1)
-            a4 = Account(email="joey@gmail.com", password=password_context.hash("321"), followable=1)
+        if not sess.exec(
+            select(Account).where(Account.email == "avealov@umich.edu")
+        ).first():
+            a1 = Account(
+                email="avealov@umich.edu",
+                password=password_context.hash("123"),
+                name="andrew ealovega",
+            )
+            a2 = Account(
+                email="smileyjo@umich.edu",
+                name="Joe smiley",
+                password=password_context.hash("456"),
+                followable=1,
+            )
+            a3 = Account(
+                email="joeysmiley33@gmail.com",
+                name="Joe smiley Jr.",
+                password=password_context.hash("789"),
+                followable=1,
+            )
+            a4 = Account(
+                email="joey@gmail.com",
+                name="Son of Joe smiley",
+                password=password_context.hash("321"),
+                followable=1,
+            )
             sess.add(a1)
             sess.add(a2)
             sess.add(a3)
             sess.add(a4)
-            f1 = Follow(email="smileyjo@umich.edu", followingEmail="joeysmiley33@gmail.com")
+            f1 = Follow(
+                email="smileyjo@umich.edu", followingEmail="joeysmiley33@gmail.com"
+            )
             f2 = Follow(email="smileyjo@umich.edu", followingEmail="joey@gmail.com")
             sess.add(f1)
             sess.add(f2)
-            m1 = Meal(email=a2.email, mealName="Chicken and Rice", category="chicken", dateMade=date(2023, 11, 1))
-            m2 = Meal(email=a3.email, mealName="Chicken and Brocolli", category="chicken", dateMade=date(2023, 11, 2))
-            m3 = Meal(email=a1.email, mealName="Macaroni and Cheese", category="pasta", dateMade=date(2023, 11, 4))
-            m4 = Meal(email=a4.email, mealName="Ceasar Salad", category="salad", dateMade=date(2023, 11, 6))
+            m1 = Meal(
+                email=a2.email,
+                mealName="Chicken and Rice",
+                category="chicken",
+                dateMade=date(2023, 11, 1),
+            )
+            m2 = Meal(
+                email=a3.email,
+                mealName="Chicken and Brocolli",
+                category="chicken",
+                dateMade=date(2023, 11, 2),
+            )
+            m3 = Meal(
+                email=a1.email,
+                mealName="Macaroni and Cheese",
+                category="pasta",
+                dateMade=date(2023, 11, 4),
+            )
+            m4 = Meal(
+                email=a4.email,
+                mealName="Ceasar Salad",
+                category="salad",
+                dateMade=date(2023, 11, 6),
+            )
             sess.add(m1)
             sess.add(m2)
             sess.add(m3)
             sess.add(m4)
-            sess.commit()  
-            
-            i1a = Ingredients(mealID = m1.mealID, ingredient="chicken", quantity=8, unit="oz")
-            i1b = Ingredients(mealID = m1.mealID, ingredient="rice", quantity=1, unit="cup")
-            i2a = Ingredients(mealID = m2.mealID, ingredient="chicken", quantity=8, unit="oz")
-            i2b = Ingredients(mealID = m2.mealID, ingredient="brocolli", quantity=4, unit="oz")
-            i3a = Ingredients(mealID = m3.mealID, ingredient="macaroni", quantity=4, unit="oz")
-            i3b = Ingredients(mealID = m3.mealID, ingredient="shredded cheese", quantity=2, unit="oz")
-            i4a = Ingredients(mealID = m4.mealID, ingredient="romaine lettuce", quantity=100, unit="grams")
-            i4b = Ingredients(mealID = m4.mealID, ingredient="croutons", quantity=30, unit="grams")
-            i4c = Ingredients(mealID = m4.mealID, ingredient="salad dressing", quantity=2, unit="tablespoons")
+            sess.commit()
+
+            i1a = Ingredients(
+                mealID=m1.mealID, 
+                ingredient="chicken", 
+                quantity=8, 
+                unit="oz"
+            )
+            i1b = Ingredients(
+                mealID=m1.mealID, 
+                ingredient="rice", 
+                quantity=1, 
+                unit="cup"
+            )
+            i2a = Ingredients(
+                mealID=m2.mealID, 
+                ingredient="chicken", 
+                quantity=8, 
+                unit="oz"
+            )
+            i2b = Ingredients(
+                mealID=m2.mealID, 
+                ingredient="brocolli", 
+                quantity=4, 
+                unit="oz"
+            )
+            i3a = Ingredients(
+                mealID=m3.mealID, 
+                ingredient="macaroni", 
+                quantity=4, 
+                unit="oz"
+            )
+            i3b = Ingredients(
+                mealID=m3.mealID, 
+                ingredient="shredded cheese", 
+                quantity=2, 
+                unit="oz"
+            )
+            i4a = Ingredients(
+                mealID=m4.mealID,
+                ingredient="romaine lettuce",
+                quantity=100,
+                unit="grams",
+            )
+            i4b = Ingredients(
+                mealID=m4.mealID, 
+                ingredient="croutons", 
+                quantity=30, 
+                unit="grams"
+            )
+            i4c = Ingredients(
+                mealID=m4.mealID,
+                ingredient="salad dressing",
+                quantity=2,
+                unit="tablespoons",
+            )
             sess.add(i1a)
             sess.add(i1b)
             sess.add(i2a)
@@ -62,20 +149,63 @@ def prepare_db():
             sess.add(mp2)
             sess.commit()
 
-            d1 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
-            d2 = MealPlanDay(mealID = m2.mealID, mealPlanID = mp1.mealPlanID)
-            d3 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
-            d4 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
-            d5 = MealPlanDay(mealID = m2.mealID, mealPlanID = mp1.mealPlanID)
-            d6 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
-            d7 = MealPlanDay(mealID = m2.mealID, mealPlanID = mp1.mealPlanID)
-            d8 = MealPlanDay(mealID = m2.mealID, mealPlanID = mp1.mealPlanID)
-            d9 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
-            d10 = MealPlanDay(mealID = m4.mealID, mealPlanID = mp1.mealPlanID)
-            d11 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
-            d12 = MealPlanDay(mealID = m4.mealID, mealPlanID = mp1.mealPlanID)
-            d13 = MealPlanDay(mealID = m2.mealID, mealPlanID = mp1.mealPlanID)
-            d14 = MealPlanDay(mealID = m1.mealID, mealPlanID = mp1.mealPlanID)
+
+            d1 = MealPlanDay(mealID=m1.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m1.mealName,
+                             weekday="monday")
+            d2 = MealPlanDay(mealID=m2.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m2.mealName,
+                             weekday="wednesday")
+            d3 = MealPlanDay(mealID=m1.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m1.mealName,
+                             weekday="tuesday")
+            d4 = MealPlanDay(mealID=m1.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m1.mealName,
+                             weekday="thursday")
+            d5 = MealPlanDay(mealID=m2.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m2.mealName,
+                             weekday="friday")
+            d6 = MealPlanDay(mealID=m1.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m1.mealName,
+                             weekday="sunday")
+            d7 = MealPlanDay(mealID=m2.mealID, 
+                             mealPlanID=mp1.mealPlanID, 
+                             mealName=m2.mealName,
+                             weekday="saturday")
+            d8 = MealPlanDay(mealID=m2.mealID, 
+                             mealPlanID=mp2.mealPlanID, 
+                             mealName=m2.mealName,
+                             weekday="monday")
+            d9 = MealPlanDay(mealID=m1.mealID, 
+                             mealPlanID=mp2.mealPlanID, 
+                             mealName=m1.mealName,
+                             weekday="saturday")
+            d10 = MealPlanDay(mealID=m4.mealID, 
+                              mealPlanID=mp2.mealPlanID, 
+                              mealName=m4.mealName,
+                              weekday="sunday")
+            d11 = MealPlanDay(mealID=m1.mealID, 
+                              mealPlanID=mp2.mealPlanID, 
+                              mealName=m1.mealName,
+                              weekday="tuesday")
+            d12 = MealPlanDay(mealID=m4.mealID, 
+                              mealPlanID=mp2.mealPlanID, 
+                              mealName=m4.mealName,
+                              weekday="wednesday")
+            d13 = MealPlanDay(mealID=m2.mealID, 
+                              mealPlanID=mp2.mealPlanID,
+                              mealName=m2.mealName, 
+                              weekday="friday")
+            d14 = MealPlanDay(mealID=m1.mealID, 
+                              mealPlanID=mp2.mealPlanID, 
+                              mealName=m1.mealName,
+                              weekday="thursday")
             sess.add(d1)
             sess.add(d2)
             sess.add(d3)
--- personalpalate/orm/model.py
+++ personalpalate/orm/model.py
@@ -4,36 +4,53 @@ from sqlmodel import SQLModel, Field, create_engine, Session, select
 from sqlalchemy import PrimaryKeyConstraint
 from enum import Enum
 
+
 class Category(Enum):
     beef = "beef"
     chicken = "chicken"
     pork = "pork"
-    meat_other = "other meat" # turkey, lamb, venison, etc.
+    meat_other = "other meat"  # turkey, lamb, venison, etc.
     seafood = "seafood"
     pasta = "pasta"
     salad = "salad"
 
+class Weekday(Enum):
+    sunday = "sunday"
+    monday = "monday"
+    tuesday = "tuesday"
+    wednesday = "wednesday"
+    thursday = "thursday"
+    friday = "friday"
+    saturday = "saturday"
+
+
 class AccountDTO(SQLModel):
     email: str = Field(primary_key=True)
+    name: str
     followable: bool = Field(default=False)
+
 
 class Account(AccountDTO, table=True):  # Subclass to avoid sending password to client
     password: str
+
 
 class Follow(SQLModel, table=True):
     email: str = Field(foreign_key="account.email")
     followingEmail: str = Field(foreign_key="account.email")
 
-    __table_args__ = (
-        PrimaryKeyConstraint("email", "followingEmail"),
-    )
+    __table_args__ = (PrimaryKeyConstraint("email", "followingEmail"),)
 
-class Meal(SQLModel, table=True):
-    mealID: Optional[int] = Field(default=None, primary_key=True)
-    email: str = Field(foreign_key="account.email")
+
+class MealDTO(SQLModel):
     mealName: str
     category: Category
     dateMade: date
+
+
+class Meal(MealDTO, table=True):
+    mealID: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(foreign_key="account.email")
+
 
 class Ingredients(SQLModel, table=True):
     ingredientID: Optional[int] = Field(default=None, primary_key=True)
@@ -42,12 +59,16 @@ class Ingredients(SQLModel, table=True):
     quantity: float
     unit: str
 
+
 class MealPlan(SQLModel, table=True):
     mealPlanID: Optional[int] = Field(default=None, primary_key=True)
     mealPlanDate: date
     email: str = Field(foreign_key="account.email")
 
+
 class MealPlanDay(SQLModel, table=True):
     dayID: Optional[int] = Field(default=None, primary_key=True)
+    weekday: Weekday
+    mealName: str
     mealID: int = Field(foreign_key="meal.mealID")
     mealPlanID: int = Field(foreign_key="mealplan.mealPlanID")
