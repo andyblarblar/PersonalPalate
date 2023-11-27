2ec5e3f87d458e4107649d25bc342aa2f6b046cc
Brett Csotty <brett.csotty@gmail.com>
2023-11-27 17:13:58 -0500

    Working on Home page

--- main.py
+++ main.py
@@ -39,11 +39,26 @@ def on_startup():
 
 @app.get("/")
 async def root(
-    request: Request, account: Annotated[AccountDTO, Depends(get_current_user)]
+    request: Request,
+    account: Annotated[AccountDTO, Depends(get_current_user)],
+    sess: Annotated[Session, Depends(db_session)]
 ):
     """Home landing page for signed in users"""
 
+    name = sess.exec(
+        select(Account)
+        .
+    ).first()
+
     return templates.TemplateResponse("home.html.jinja", {"request": request})
+
+
+@app.get("/test")
+async def test_page(
+    request: Request
+):
+    """Testing page for new pages in development"""
+    return templates.TemplateResponse("new.home.html.jinja", {"request": request})
 
 
 class FollowData(BaseModel):
